a54ddc37082cbafd1bcba5956ed77df20471b5cf
vw-liane <42220158+vw-liane@users.noreply.github.com>
2020-04-02 13:41:44 -0500

    3/4 version <dynamic_class>

--- Dynamic-Class-Calling-04-01-20/SO_dynamic_class_call.py
+++ Dynamic-Class-Calling-04-01-20/SO_dynamic_class_call.py
@@ -33,23 +33,56 @@ class Cucumber(Vegetable):
     def __str__(self):
         return f"{self.name} object"
 
-def extract_abbv():
-    df_abvs = {}     # holds the DFs of abvs of individual grapes -or- cucumbers
+def extract_ser():
+    sub_cls_ser = {}     # holds the series of grapes -or- cucumbers
     classes_path = Path('Classes')  # starting folder
     for super_class in classes_path.iterdir(): # Path of Fruit -or- Vegetable Folder
         for sub_class in super_class.iterdir(): # Path of <grapes.csv> -or- <cucumbers.csv>
-            f_name_str = str(sub_class.name)
+            df_csv = pd.read_csv(sub_class)  # dataframe assignment
+            df_ser = df_csv.iloc[:, 0]      # series assignment
 
-    return classes_path, df_abvs
+            sub_cls_ser[sub_class.stem] = df_ser  # [stem] = series
+    return classes_path, sub_cls_ser
 
-def make_objs(cls_fldr_pth):
 
-
+def make_objs(cls_fldr_pth, sub_ser):
     for super_class in cls_fldr_pth.iterdir():
+        make_veg = lambda veg: Vegetable()  # INSERT varying named lambda function & CONSTRUCTOR CALL
+                                            #  later applied to series
+        ## APPLY super_constr
+        for str_key, ser_val in sub_ser.items():
+            print(f"Check DF str_key: {str_key}")  # use <str_key> to check which is super class
+            obj_df = ser_val.apply(make_veg)     # inside .apply() would change based on super_class
+
+            print(f"After SUPER .apply()::\nCheck OBJ_DF Contents\n{obj_df}\n")
+
         for sub_class in super_class.iterdir():
-            # how can I call here the coorresponding SUB & SUPER
-            # class constructors?
-            print(f"File Stem: {sub_class.stem}")  # no errors when test running
+            sub_constr = sub_class.stem      # based on this call subclass constructor
 
+            make_cuc = lambda veg: Cucumber()  # INSERT varying named lambda function & CONSTRUCTOR CALL
+                                                #  later applied to the series
+            for str_key, df_val in sub_ser.items():
+                ## APPLY sub_constr
+                print(f"Check DF str_key: {str_key}")
+                obj_df = df_val.apply(make_cuc)    # inside .apply() would change based on sub_class
 
-make_objs()
+                print(f"After SUB .apply()::\nCheck OBJ_DF Contents\n{obj_df}\n")
+
+if __name__ == "__main__":
+        classes_flder_path, sub_cls_series = extract_ser()
+        make_objs(classes_flder_path, sub_cls_series)
+        
+        
+        
+## WHAT I HAVE TRIED
+## to write at the beginning of make_objs()
+###################
+
+# make lambda function
+#lam_func = "make_" + (str(sub_class.stem))[:3]   # first 3 char
+#cls_call = (str(sub_class.stem)).capitalize()
+#lam_func = lambda fst_3_char: cls_call()       # error string not callable
+
+# Is there a way to call the Class constructor
+# by checking if something matches the class name?
+# .... for example, if <cls_call> matches the name?
